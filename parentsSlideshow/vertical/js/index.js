1b19729da23083a70b9fd48d5af9831b5d760270
Ying Zhang <ying.zhang@meredith.com>
2016-03-17 12:42:09 -0700

    Add more for vertical slideshow

--- parentsSlideshow/vertical/js/index.js
+++ parentsSlideshow/vertical/js/index.js
@@ -1,20 +1,63 @@
-$('.lazy').each(function() {
+var $slideContainer = $('#slideContainer');
+
+$('.lazy').each(function () {
   $(this).attr('src', $(this).data('src'));
 });
 
-var $slideContainer = $('#slideContainer');
+var initialPage = $.url(window.location).param('page') || $.url(window.location).fparam('page');
+console.log('initialPage', initialPage);
+if (initialPage) {
+  var prevPage = initialPage - 1;
+  var $nav = $('[data-slide-number="' + prevPage + '"] .nextSlide');
+  if ($nav.length) {
+    console.log($nav);
+    moveSlide($nav, 'next');
+  }
+}
 
 function moveSlide($nav, direction) {
   var $slide = $nav.parents('.slide');
   var currentSlideNumber = $slide.data('slide-number');
+  leaveSlide($slide);
   //console.log('currentSlideNumber', currentSlideNumber);
   var transform = -(currentSlideNumber) * 100;
   if('previous' === direction) {
     transform = -(currentSlideNumber-2) * 100;
     //console.log('transform', transform);
+    var prevSlide = currentSlideNumber - 1;
+    var $toSlide = $('[data-slide-number="' + prevSlide + '"]');
   }
+  else {
+    var nextSlide = currentSlideNumber + 1;
+    var $toSlide = $('[data-slide-number="' + nextSlide + '"]');
+  }
+  preloadSlide($toSlide);
   $slideContainer.css('-webkit-transform', 'translateY(' + transform + '%)');
   $slide.scrollTop(0);
+}
+
+function leaveSlide($slide) {
+
+}
+
+function preloadSlide($slide) {
+  if ($slide.length) {
+    if ($slide.hasClass('slide--video')) {
+      var $videoPlayer = $slide.find('.videoPlayer');
+      embedVideo($videoPlayer);
+    }
+  }
+
+}
+
+function embedVideo($videoPlayer) {
+  var videoUrl = $videoPlayer.data('videoembedurl');
+  var width = $videoPlayer.outerWidth();
+  var height = $videoPlayer.outerHeight();
+  videoUrl += '&enablem4=true';
+  $videoPlayer.html('<iframe width="100%" height="100%" scrolling="no" seamless="seamless" src="' + videoUrl + '" frameborder="0"  allowfullscreen></iframe>');
+  $videoPlayer.addClass('embeddedVideoAdded');
+  $videoPlayer.parents('.slideImageInnerWrapper').addClass('embeddedVideoAdded');
 }
 
 
