f138451baa1eafcb10887135993c0492c2fb4771
Ying Zhang <ying.zhang@meredith.com>
2018-03-18 10:47:39 -0700

    Add bhg project critical.

---
+++ bhgPerformance/generateCritical.js
@@ -0,0 +1,9 @@
+const critical = require('critical');
+critical.generate({
+  inline: true,
+  base: 'project/',
+  src: 'project1.html',
+  dest: 'project1-critical.html',
+  width: 375,
+  height: 667
+});
