b7c8a6223f27bcfd41fc8843a208809a9e5b906b
Ying Zhang <ying.zhang@meredith.com>
2018-03-18 10:48:57 -0700

    Revert "Add bhg project critical."
    This reverts commit f138451baa1eafcb10887135993c0492c2fb4771.

--- bhgPerformance/generateCritical.js
+++
@@ -1,9 +0,0 @@
-const critical = require('critical');
-critical.generate({
-  inline: true,
-  base: 'project/',
-  src: 'project1.html',
-  dest: 'project1-critical.html',
-  width: 375,
-  height: 667
-});
